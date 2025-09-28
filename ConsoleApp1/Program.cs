457ceb7b2aca06a8659952d4a697d2cf1a341a47
TranQuangHiep8386 <hieptran170298@gmail.com>
2025-09-28 22:34:49 +0700

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using TuViLib;
+
+namespace TuViConsole
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Đặt encoding UTF-8 cho console
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+
+            Console.Write("Họ tên: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Ngày sinh (dd/MM/yyyy): ");
+            DateTime dob = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", null);
+
+            Console.Write("Giới tính (Nam/Nữ/Khác): ");
+            string gender = Console.ReadLine();
+
+            string report = TuViCalculator.GetReport(name, dob, gender);
+            Console.WriteLine("\n==== TỬ VI ====");
+            Console.WriteLine(report);
+
+            Console.WriteLine("\nNhấn phím bất kỳ để thoát...");
+            Console.ReadKey();
+        }
+    }
+}
